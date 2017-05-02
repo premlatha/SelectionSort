ae13c5e932c44a771726b08898ed4cc1e68fc590
PremlathaShendre <premlathashendre@gmail.com>
2017-05-02 14:13:34 -0500

    SelectionSort

---
+++ SelectionSort.Test/SelectionSortTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SelectionSort.Test
+{
+    [TestFixture]
+    public class SelectionSortTest
+    {
+        [Test]
+        public void PerformSort()
+        {
+            int[] unsortedArr = { 66, 34, 1, 90, 88 };
+            int[] actualSortedArr = { 1,34,66,88,90};
+            int[] expectedSortedArr = Program.PerformSelectionSort(unsortedArr);
+            Assert.AreEqual(expectedSortedArr, actualSortedArr, "Array is sorted");
+        }
+
+    }
+}
